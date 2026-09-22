1edeb1d1012d8270e5c9ae237edf6632b4e7b840
giu-mont <giulia-montagnani@studio.unibo.it>
2026-09-22 16:24:03 +0200

    Implemented src.[...] for import

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# This file makes the "src" directory a Python package,
+# allowing functions defined in its modules to be imported
+# directly into notebooks, for example to import my_function from the file functions.py:
+#
+# from src.functions import my_function
